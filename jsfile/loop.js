ed98571778cf0a1f73d2fb33e670ac0ebe8270a5
Yogesh Mundhe <yogesh.mundhe@waiin.com>
2022-09-29 15:13:56 +0530

    done

---
+++ jsfile/loop.js
@@ -0,0 +1,61 @@
+// let counter=0;
+// while(counter<11){
+//     console.log("Hello");
+// }
+
+// console.log("hii")
+
+// document.write("sjkdfsab");
+
+// let counter = 1;
+// while(counter < 11){
+//     document.write("Hello <br>"); 
+//     counter++;
+// }
+
+// let counter=0;
+// let sum=0;
+
+// while(counter<250){
+//     if (counter%2==0){
+//         sum = sum+counter;
+
+//     }
+//     counter++;
+
+// }
+// document.write(sum);
+
+
+
+
+// let age = prompt("Enter Your age:")
+// if (age >= 18) {
+//     document.write("You can Vote...!")
+// }
+// else{
+//     document.write("You can't Vote!")
+// }
+
+
+
+// confirm box example
+
+// let response = confirm(" Are you wan't to delete?")
+// if (response){
+//     document.write("Deleted.")
+// }
+// else{
+//     document.write("Not deleted.")
+// }
+
+//----------------------------String Manipulation------------//
+
+
+//------------Array----------------------------------------------//
+
+
+let arr = ['C#', 'HTML', 'CSS', 'Bootstrap', 'Angular'];
+document.write(arr+'<br>');
+document.write(arr.length);
+
